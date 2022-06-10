6bd786dda56f63505256fab827d7d1d02cda5847
Eriche Gonzales <erichelouise@gmail.com>
2022-06-10 02:38:11 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped a ${gifts[i]} and added a bow!`)
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const arrayMessages = [];
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        arrayMessages.push(message);
+    }
+    return arrayMessages;
+};
+
+function countDown(count) {
+    while (count > -1) {
+        console.log(count);
+        count--;
+    }
+}
+
+
+
